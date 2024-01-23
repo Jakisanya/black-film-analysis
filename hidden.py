e51a00becae247b1052e40cdfc0438ce84137c9a
Jordan Akisanya <70146756+Jakisanya@users.noreply.github.com>
2024-01-23 14:57:31 +0000

    Update hidden.py
    Removing private info

--- hidden.py
+++ hidden.py
@@ -1,6 +1,6 @@
-secrets = {"tmdb": {"tmdb_api_key": '78d8d7264a394221f7bc34cb8a55a404'},
-           "omdb": {"omdb_api_key": '499f9e4b'},
+secrets = {"tmdb": {"tmdb_api_key": ''},
+           "omdb": {"omdb_api_key": ''},
            "alchemy": {"connection_string":
-                       'sqlite:////C:/Users/ec21009/OneDrive - Queen Mary, University of London/FilmAnalysisDB.db'}}
+                       ''}}
 
 
